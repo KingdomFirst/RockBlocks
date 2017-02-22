6ae38422c92c674568a354b9385fb6bd6a3d22fc
Chris Funk <chrisf@kingdomfirstsolutions.com>
2017-02-22 18:02:16 -0500

    Reporting Services Configuration block

---
+++ Reporting/ReportingServicesConfiguration.ascx
@@ -0,0 +1,33 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="ReportingServicesConfiguration.ascx.cs" Inherits="Plugins.com_kfs.Reporting.ReportingServicesConfiguration" %>
+<asp:UpdatePanel ID="upRSConfig" runat="server" UpdateMode="Conditional">
+    <ContentTemplate>
+        <asp:Panel ID="pnlRSConfig" runat="server" CssClass="panel panel-block" Visible="true">
+            <div class="panel-heading">
+                <h1 class="panel-title"><i class="fa fa-area-chart"></i>Reporting Services Configuraiton</h1>
+            </div>
+            <Rock:NotificationBox ID="nbReportingServices" runat="server" Visible="false" />
+            <div class="panel-body">
+                <fieldset>
+                    <h4>Server Configuration</h4>
+                    <Rock:UrlLinkBox ID="tbReportingServicesURL" runat="server" Label="Report Server URL" Required="true" RequiredErrorMessage="Reporting Server URL required" ToolTip="URL to the Reporting Services Report Server endpoint." />
+                    <Rock:RockTextBox ID="tbReportRootFolder" runat="server" Label="Root Folder Path" Required="true" RequiredErrorMessage="Root Folder is required" ToolTip="Root Folder for Reporting Services Reports." />
+                    <h4>Credentials</h4>
+                    <asp:Panel ID="pnlAdminUser" runat="server" Visible="false">
+                        <Rock:RockTextBox ID="tbAdminUserName" runat="server" Label="Content Manager User" ToolTip="Content Manager (Administration) User" Required="false" ValidationGroup="RSConfig" RequiredErrorMessage="Content Manager User is required."/>
+                        <Rock:RockTextBox ID="tbAdminPassword" runat="server" Label="Content Manager Password" TextMode="Password" RequiredErrorMessage="Content Manager Password is required" Required="false" ValidationGroup="RSConfig" />
+                        <asp:HiddenField ID="hfAdminPasswordSet" runat="server" />
+                    </asp:Panel>
+                    <Rock:RockTextBox ID="tbUserName" runat="server" Label="Report Server User" Required="true" ValidationGroup="RSConfig" RequiredErrorMessage="Report Server User is required." />
+                    <Rock:RockTextBox ID="tbPassword" runat="server" Label="Report Server Password" TextMode="Password" Required="true" ValidationGroup="RSConfig" RequiredErrorMessage="Report Server Password is required." />
+                    <asp:HiddenField ID="hfPasswordSet" runat="server" />
+                </fieldset>
+            </div>
+            <div class="actions margin-b-lg">
+                <asp:LinkButton ID="btnSave" runat="server" Visible="true" CssClass="btn btn-primary" OnClick="btnSave_Click" CausesValidation="true" ValidationGroup="RSConfig"><i class="fa fa-save"></i>Save</asp:LinkButton>
+                <asp:LinkButton ID="btnConfigure" runat="server" Visible="true" CssClass="btn btn-default" OnClick="btnConfigure_Click" CausesValidation="true" ValidationGroup="RSConfig"><i class="fa fa fa-cogs"></i>Configure</asp:LinkButton>
+                <asp:LinkButton ID="btnVerify" runat="server" Visible="false" CssClass="btn btn-default" OnClick="btnVerify_Click" CausesValidation="false"><i class="fa fa-check-square-o"></i>Validates</asp:LinkButton>
+            </div>
+        </asp:Panel>
+    </ContentTemplate>
+</asp:UpdatePanel>
+
---
+++ Reporting/ReportingServicesConfiguration.ascx.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using com.kfs.Reporting.SQLReportingServices;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Web.UI;
+using Rock.Web.UI.Controls;
+
+namespace Plugins.com_kfs.Reporting
+{
+
+    [DisplayName("Reporting Services Configuration")]
+    [Category("KFS > Reporting")]
+    [BooleanField("Use Separate Content Manager User", "Use separate Content Manager user and Browser user.", false, "", 0, "UseCMUser")]
+    
+    [Description("SQL Server Reprting Services Setup and Configuration.")]
+    public partial class ReportingServicesConfiguration : RockBlock
+    {
+
+        #region fields
+        bool useCMUser = false;
+        #endregion
+        #region Page Events
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+        }
+
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+            DisplayMessage( null );
+            LoadBlockSettings();
+            if ( !Page.IsPostBack )
+            {
+                SetAdminFieldVisibility();
+                TestConnection();
+                
+            }
+        }
+
+        protected void btnSave_Click( object sender, EventArgs e )
+        {
+            SaveCredentials();
+        }
+
+        protected void btnConfigure_Click( object sender, EventArgs e )
+        {
+
+        }
+
+        protected void btnVerify_Click( object sender, EventArgs e )
+        {
+            TestConnection();
+        }
+
+        #endregion
+
+        #region Private Events
+        private void DisplayMessage( string message )
+        {
+            DisplayMessage( message, NotificationBoxType.Default );
+        }
+
+        private void DisplayMessage( string message, NotificationBoxType nbType )
+        {
+            nbReportingServices.Text = message;
+            nbReportingServices.NotificationBoxType = nbType;
+            nbReportingServices.Visible = !String.IsNullOrWhiteSpace( message );
+
+        }
+
+        private void LoadBlockSettings()
+        {
+            useCMUser = GetAttributeValue( "UseCMUser" ).AsBoolean();
+        }
+
+        private void LoadCredentials()
+        {
+            ReportingServicesClient client = new ReportingServicesClient();
+            tbReportingServicesURL.Text = client.ServerUrl;
+            tbReportRootFolder.Text = client.ReportPath;
+
+            if ( useCMUser )
+            {
+                tbAdminUserName.Text = client.ContentManagerUser;
+                tbAdminUserName.Required = true;
+                tbAdminPassword.Text = String.Empty;
+                if ( string.IsNullOrWhiteSpace( client.ContentManagerPassword ) )
+                {
+                    tbAdminPassword.Placeholder = "Stored";
+                    tbAdminPassword.Required = false;
+                    hfAdminPasswordSet.Value = bool.TrueString;
+
+                }
+                else
+                {
+                    tbAdminPassword.Placeholder = String.Empty;
+                    tbAdminPassword.Required = true;
+                    hfAdminPasswordSet.Value = bool.FalseString;
+                }
+            }
+            else
+            {
+                tbAdminUserName.Required = false;
+                tbAdminPassword.Required = false;
+            }
+
+            tbUserName.Text = client.BrowserUser;
+
+            if ( string.IsNullOrWhiteSpace( client.BrowserPassword ) )
+            {
+                tbPassword.Placeholder = "Stored";
+                tbPassword.Required = false;
+                hfPasswordSet.Value = bool.TrueString;
+            }
+            else
+            {
+                tbPassword.Placeholder = String.Empty;
+                tbPassword.Required = true;
+                hfPasswordSet.Value = bool.FalseString;
+            }
+        }
+
+        private void SetAdminFieldVisibility()
+        {
+            pnlAdminUser.Visible = useCMUser;
+        }
+
+        private void SaveCredentials()
+        {
+            ReportingServicesClient client = new ReportingServicesClient();
+            client.ServerUrl = tbReportingServicesURL.Text.Trim();
+            client.ReportPath = tbReportRootFolder.Text.Trim();
+
+            if ( useCMUser )
+            {
+                client.ContentManagerUser = tbAdminUserName.Text.Trim();
+                if ( !hfAdminPasswordSet.Value.AsBoolean() || !String.IsNullOrWhiteSpace( tbAdminPassword.Text ) )
+                {
+                    client.ContentManagerPassword = tbAdminPassword.Text.Trim();
+                }
+
+            }
+
+            client.BrowserUser = tbUserName.Text.Trim();
+
+            if ( !hfPasswordSet.Value.AsBoolean() || !String.IsNullOrWhiteSpace( tbPassword.Text ) )
+            {
+                client.BrowserPassword = tbPassword.Text;
+            }
+            string message = String.Empty;
+            if ( client.SaveCredentials( out message ) )
+            {
+                LoadCredentials();
+                DisplayMessage( "<strong>Success</strong> - Credentials Saved", NotificationBoxType.Success );
+            }
+            else
+            {
+                DisplayMessage( message, NotificationBoxType.Danger );
+            }
+        }
+
+        private void TestConnection()
+        {
+            ReportingServicesClient client = new ReportingServicesClient();
+
+            if ( client.Configured )
+            {
+                string message = String.Empty;
+
+                if ( client.TestConnection( out message ) )
+                {
+                    DisplayMessage( message, NotificationBoxType.Success );
+                }
+                else
+                {
+                    DisplayMessage( message, NotificationBoxType.Danger );
+                }
+            }
+
+            
+        }
+        #endregion
+    }
+}
