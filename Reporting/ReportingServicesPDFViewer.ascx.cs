e752d6e91b865fed7aed921b954801205451828b
Chris Funk <chrisf@kingdomfirstsolutions.com>
2017-05-09 17:17:29 -0400

    add the ability to directly download a pdf

--- Reporting/ReportingServicesPDFViewer.ascx.cs
+++ Reporting/ReportingServicesPDFViewer.ascx.cs
@@ -16,9 +16,11 @@ namespace com.kfs.Reporting.SQLReportingServices
     [Category( "KFS > Reporting" )]
     [TextField( "Report Path", "Relative Path to Reporting Services Report. Used in single report mode, and will overide \"ReportPath\" page parameter.", false, "", "Report Configuration", 0, "ReportPath" )]
     [KeyValueListField( "Report Parameters", "Report Parameters.", false, "", "Name", "Value", Category = "Report Configuration", Order = 1, Key = "ReportParameters" )]
-
+    [BooleanField("Show PDF Viewer", "A flag that determines if the full PDF Viewer block should be rendered or only return the report pdf. Default is true.", true, "Advanced", 0, "ShowReportViewer")]
     public partial class ReportingServicesPDFViewer : RockBlock
     {
+
+        bool mRenderPDFOnly = false;
         #region Page Event
         protected override void OnInit( EventArgs e )
         {
@@ -28,6 +30,7 @@ namespace com.kfs.Reporting.SQLReportingServices
         protected override void OnLoad( EventArgs e )
         {
             base.OnLoad( e );
+            mRenderPDFOnly = !GetAttributeValue( "ShowReportViewer" ).AsBoolean();
             SetError( null, null );
             if ( !Page.IsPostBack )
             {
@@ -88,6 +91,7 @@ namespace com.kfs.Reporting.SQLReportingServices
 
         private void LoadReport()
         {
+            pnlPdfViewer.Visible = false;
             phViewer.Controls.Clear();
             ReportingServicesProvider provider = new ReportingServicesProvider();
             string reportPath = GetReportPath( provider );
@@ -95,7 +99,6 @@ namespace com.kfs.Reporting.SQLReportingServices
 
             if ( String.IsNullOrWhiteSpace( reportPath ) )
             {
-                pnlPdfViewer.Visible = false;
                 SetError( "Report Path Error", "Report Path is required." );
                 return;
             }
@@ -104,7 +107,6 @@ namespace com.kfs.Reporting.SQLReportingServices
 
             if ( rsItem == null )
             {
-                pnlPdfViewer.Visible = false;
                 SetError( "Report Path Error", "Report Not Found" );
                 return;
             }
@@ -141,24 +143,78 @@ namespace com.kfs.Reporting.SQLReportingServices
 
             StringBuilder urlBuilder = new StringBuilder();
             urlBuilder.AppendFormat( "{0}?reportPath={1}",
-                ResolveRockUrl( "~/Plugins/com_kfs/Reporting/GetReportingServicesPDF.ashx" ),
+                ResolveRockUrlIncludeRoot( "~/Plugins/com_kfs/Reporting/GetReportingServicesPDF.ashx" ),
                 reportPath );
             foreach ( var param in paramValues )
             {
                 urlBuilder.AppendFormat( "&{0}={1}", param.Key, param.Value );
             }
 
-            string iframeTag = string.Format( "<iframe src=\"{0}\" id=\"ifReportPDF\" class=\"col-sm-12\"></iframe>", urlBuilder.ToString() );
-            phViewer.Controls.Add( new LiteralControl( iframeTag ) );
-            pnlPdfViewer.Visible = true;
+            if ( !mRenderPDFOnly )
+            {
+                string iframeTag = string.Format( "<iframe src=\"{0}\" id=\"ifReportPDF\" class=\"col-sm-12\"></iframe>", urlBuilder.ToString() );
+                phViewer.Controls.Add( new LiteralControl( iframeTag ) );
+                pnlPdfViewer.Visible = true;
+            }
+            else
+            {
+                byte[] pdfBytes = GetPDFStream( urlBuilder.ToString() );
+                base.Response.Clear();
+                string dispositionType = String.Empty;
 
+                if ( pdfBytes == null )
+                {
+                    base.Response.StatusCode = 404;
+                }
+                else
+                {
+                    base.Response.AppendHeader( "content-length", ( pdfBytes.Length ).ToString() );
+                    //base.Resp
+                    base.Response.AppendHeader( "content-disposition", string.Format( "filename={0}.pdf", rsItem.Name ) );
+                    base.Response.ContentType = "applicaton/pdf";
+                    base.Response.OutputStream.Write( pdfBytes, 0, pdfBytes.Length );
+                }
+                base.Response.Flush();
+                base.Response.Close();
+                base.Response.End();
+
+            }
+
+            
+        }
+
+
+        private byte[] GetPDFStream( string url )
+        {
+            byte[] pdfBytes = null;
+
+            System.Net.WebRequest req = System.Net.WebRequest.Create( url );
+            req.Method = "GET";
+
+            System.Net.HttpWebResponse resp = (System.Net.HttpWebResponse)req.GetResponse();
+
+            if ( resp.StatusCode == System.Net.HttpStatusCode.OK && resp.ContentType.IndexOf( "Application/PDF", StringComparison.InvariantCultureIgnoreCase ) >= 0 )
+            {
+                pdfBytes = resp.GetResponseStream().ReadBytesToEnd();
+            }
+
+            return pdfBytes;
         }
 
         private void SetError( string title, string error )
         {
-            nbError.Title = title;
-            nbError.Text = error;
-            nbError.Visible = !String.IsNullOrWhiteSpace( error );
+            if ( !mRenderPDFOnly )
+            {
+                nbError.Title = title;
+                nbError.Text = error;
+                nbError.Visible = !String.IsNullOrWhiteSpace( error );
+            }
+            else if(!String.IsNullOrWhiteSpace(error))
+            {
+                string exMessage = string.Format( "{0}{1}{2}", title, String.IsNullOrWhiteSpace(title) ? "" : " - ", error );
+                throw new Exception( exMessage.Trim() );
+
+            }
         }
         #endregion
 
