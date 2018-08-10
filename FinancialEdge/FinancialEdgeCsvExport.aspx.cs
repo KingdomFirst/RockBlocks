fba56abfe4208e15fbf3c5cfffcd1d632ddd5631
Trey Hendon <treyh@kingdomfirstsolutions.com>
2018-08-10 01:53:59 -0400

    Added Financial Edge CSV Exporter

---
+++ FinancialEdge/FinancialEdgeCsvExport.aspx.cs
@@ -0,0 +1,33 @@
+﻿namespace RockWeb.Plugins.com_kfs.FinancialEdge
+{
+    using System;
+    using System.IO;
+    using System.Text;
+
+    public partial class FinancialEdgeCsvExport : System.Web.UI.Page
+    {
+        protected void Page_Load( object sender, EventArgs e )
+        {
+            if ( !Request.IsAuthenticated )
+            {
+                litPlaceholder.Text = "You must be logged in to export batches.";
+                return;
+            }
+
+            if ( Session["FinancialEdgeCsvExport"] != null && !string.IsNullOrEmpty( Session["FinancialEdgeCsvExport"].ToString() ) && Session["FinancialEdgeBatchId"] != null && !string.IsNullOrEmpty( Session["FinancialEdgeBatchId"].ToString() ) )
+            {
+                var filename = string.Format( "FinancialEdge_{0}.csv", Session["FinancialEdgeBatchId"] );
+                var output = Session["FinancialEdgeCsvExport"].ToString();
+                Session["FinancialEdgeCsvExport"] = null;
+                Session["FinancialEdgeBatchId"] = null;
+                var ms = new MemoryStream( Encoding.ASCII.GetBytes( output ) );
+                Response.ClearContent();
+                Response.ClearHeaders();
+                Response.ContentType = "text/csv";
+                Response.AddHeader( "Content-Disposition", string.Format( "attachment; filename={0}", filename ) );
+                ms.WriteTo( Response.OutputStream );
+                Response.End();
+            }
+        }
+    }
+}
