0379d92d516db1147b4c9d3158122a522a7a8879
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2021-08-03 16:31:48 -0600

    Care Entry block created.

---
+++ StepsToCare/CareEntry.ascx
@@ -0,0 +1,86 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeFile="CareEntry.ascx.cs" Inherits="RockWeb.Plugins.rocks_kfs.StepsToCare.CareEntry" %>
+
+<asp:UpdatePanel runat="server" ID="upnlCareEntry">
+    <ContentTemplate>
+        <asp:Panel ID="pnlView" runat="server" CssClass="panel panel-block">
+            <asp:HiddenField ID="hfCareNeedId" runat="server" />
+            <div class="panel-heading">
+                <h1 class="panel-title pull-left"><i class="fa fa-hand-holding"></i>Care Need</h1>
+
+                <div class="panel-labels">
+                    <Rock:HighlightLabel ID="hlStatus" runat="server" LabelType="Default" Text="Pending" />
+                </div>
+            </div>
+            <Rock:PanelDrawer ID="pdAuditDetails" runat="server"></Rock:PanelDrawer>
+            <div class="panel-body">
+                <asp:ValidationSummary ID="valValidation" runat="server" HeaderText="Please correct the following:" CssClass="alert alert-validation" />
+
+                <div class="">
+                    <div class="row">
+                        <div class="col-md-3">
+                            <Rock:DatePicker ID="dpDate" runat="server" Label="Date" SourceTypeName="rocks.kfs.StepsToCare.Model.CareNeed, rocks.kfs.StepsToCare" PropertyName="DateEntered" />
+                        </div>
+                        <div class="col-md-3">
+                            <Rock:PersonPicker ID="ppSubmitter" runat="server" Label="Submitter" Visible="false" SourceTypeName="rocks.kfs.StepsToCare.Model.CareNeed, rocks.kfs.StepsToCare" PropertyName="SubmitterPersonAlias" />
+                        </div>
+                        <div class="col-md-3">
+                            <Rock:CampusPicker ID="cpCampus" runat="server" Label="Campus" />
+                        </div>
+                        <div class="col-md-3">
+                            <Rock:DefinedValuePicker ID="dvpStatus" runat="server" Label="Status" SourceTypeName="rocks.kfs.StepsToCare.Model.CareNeed, rocks.kfs.StepsToCare" PropertyName="StatusValueId" Required="true" />
+                        </div>
+                    </div>
+                </div>
+
+                <Rock:PanelWidget ID="wpRequestor" runat="server" Title="Requestor" Expanded="true" CssClass="margin-t-md">
+                    <div class="row">
+                        <div class="col-md-8">
+                            <Rock:PersonPicker ID="ppPerson" runat="server" Label="Person" OnSelectPerson="ppPerson_SelectPerson" SourceTypeName="rocks.kfs.StepsToCare.Model.CareNeed, rocks.kfs.StepsToCare" PropertyName="PersonAlias" />
+                        </div>
+                    </div>
+
+                    <asp:Panel runat="server" ID="pnlNewPersonFields">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <Rock:RockTextBox ID="dtbFirstName" runat="server" Label="First Name" />
+                            </div>
+                            <div class="col-md-4">
+                                <Rock:RockTextBox ID="dtbLastName" runat="server" Label="Last Name" />
+                            </div>
+                            <div class="col-md-4">
+                                <Rock:EmailBox ID="ebEmail" runat="server" Label="Email" />
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="col-md-4">
+                                <Rock:PhoneNumberBox ID="pnbHomePhone" runat="server" Label="Home Phone" />
+                            </div>
+                            <div class="col-md-4">
+                                <Rock:PhoneNumberBox ID="pnbCellPhone" runat="server" Label="Cell Phone" />
+                            </div>
+                            <div class="col-md-4">
+                                <Rock:LocationAddressPicker ID="lapAddress" runat="server" Label="Address" />
+                            </div>
+                        </div>
+                    </asp:Panel>
+                </Rock:PanelWidget>
+
+                <Rock:PanelWidget ID="pwDetails" runat="server" Title="Need Details" Expanded="true">
+                    <Rock:DefinedValuePicker ID="dvpCategory" runat="server" Label="Category" SourceTypeName="rocks.kfs.StepsToCare.Model.CareNeed, rocks.kfs.StepsToCare" PropertyName="CategoryValueId" Required="true" />
+
+                    <Rock:DataTextBox ID="dtbDetailsText" runat="server" Label="Description of Need" TextMode="MultiLine" Rows="4" SourceTypeName="rocks.kfs.StepsToCare.Model.CareNeed, rocks.kfs.StepsToCare" PropertyName="Details" />
+
+                    <Rock:DynamicPlaceholder ID="phAttributes" runat="server" />
+                </Rock:PanelWidget>
+
+                <asp:LinkButton ID="lbSave" runat="server" AccessKey="s" ToolTip="Alt+s" Text="Save" CssClass="btn btn-primary" OnClick="lbSave_Click" />
+                <asp:LinkButton ID="lbCancel" runat="server" AccessKey="c" ToolTip="Alt+c" Text="Cancel" CssClass="btn btn-link" CausesValidation="false" OnClick="lbCancel_Click" />
+            </div>
+
+        </asp:Panel>
+
+        <Rock:ConfirmPageUnload ID="confirmExit" runat="server" ConfirmationMessage="Changes have been made to this care need that have not yet been saved." Enabled="false" />
+
+    </ContentTemplate>
+</asp:UpdatePanel>
---
+++ StepsToCare/CareEntry.ascx.cs
@@ -0,0 +1,407 @@
+﻿// <copyright>
+// Copyright 2021 by Kingdom First Solutions
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+
+using Rock;
+using Rock.Attribute;
+using Rock.Data;
+using Rock.Model;
+using Rock.Web.Cache;
+using Rock.Web.UI.Controls;
+using rocks.kfs.StepsToCare.Model;
+
+namespace RockWeb.Plugins.rocks_kfs.StepsToCare
+{
+    #region Block Attributes
+
+    [DisplayName( "Care Entry" )]
+    [Category( "KFS > Steps To Care" )]
+    [Description( "Care entry page for KFS Steps to Care package. Used for adding and editing care needs " )]
+
+    #endregion
+
+    #region Block Settings
+
+    [BooleanField( "Allow New Person Entry", "Should you be able to enter a new person from the care entry form?", false, key: "AllowNewPerson" )]
+
+    #endregion
+
+    public partial class CareEntry : Rock.Web.UI.RockBlock
+    {
+
+        bool _allowNewPerson = false;
+
+        #region Base Control Methods
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
+        /// </summary>
+        /// <param name="e">An <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnInit( EventArgs e )
+        {
+            base.OnInit( e );
+
+            this.BlockUpdated += Block_BlockUpdated;
+            this.AddConfigurationUpdateTrigger( upnlCareEntry );
+
+            _allowNewPerson = GetAttributeValue( "AllowNewPerson" ).AsBoolean();
+        }
+
+        /// <summary>
+        /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
+        /// </summary>
+        /// <param name="e">The <see cref="T:System.EventArgs" /> object that contains the event data.</param>
+        protected override void OnLoad( EventArgs e )
+        {
+            base.OnLoad( e );
+
+            if ( !Page.IsPostBack )
+            {
+                cpCampus.Campuses = CampusCache.All();
+                ShowDetail( PageParameter( "CareNeedId" ).AsInteger() );
+            }
+            else
+            {
+                var rockContext = new RockContext();
+                CareNeed item = new CareNeedService( rockContext ).Get( hfCareNeedId.ValueAsInt() );
+                if ( item == null )
+                {
+                    item = new CareNeed();
+                }
+                item.LoadAttributes();
+
+                phAttributes.Controls.Clear();
+                Helper.AddEditControls( item, phAttributes, false, BlockValidationGroup, 2 );
+
+                confirmExit.Enabled = true;
+            }
+        }
+
+        #endregion
+
+        #region Events
+
+        /// <summary>
+        /// Handles the BlockUpdated event of the control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void Block_BlockUpdated( object sender, EventArgs e )
+        {
+            ShowDetail( PageParameter( "CareNeedId" ).AsInteger() );
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbSave control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbSave_Click( object sender, EventArgs e )
+        {
+            if ( Page.IsValid )
+            {
+                RockContext rockContext = new RockContext();
+                CareNeedService careNeedService = new CareNeedService( rockContext );
+
+                CareNeed careNeed = null;
+                int careNeedId = PageParameter( "CareNeedId" ).AsInteger();
+
+                if ( !careNeedId.Equals( 0 ) )
+                {
+                    careNeed = careNeedService.Get( careNeedId );
+                }
+
+                if ( careNeed == null )
+                {
+                    careNeed = new CareNeed { Id = 0 };
+                }
+
+                careNeed.Details = dtbDetailsText.Text;
+                careNeed.CampusId = cpCampus.SelectedCampusId;
+
+                //var FirstName = dtbFirstName.Text;
+                //var LastName = dtbLastName.Text;
+                //var Email = ebEmail.Text;
+
+                //if ( lapAddress.Location != null )
+                //{
+                //    LocationId = lapAddress.Location.Id;
+                //}
+
+                //var HomePhoneNumber = pnbHomePhone.Number;
+                //var CellPhoneNumber = pnbCellPhone.Number;
+
+                careNeed.PersonAliasId = ppPerson.PersonAliasId;
+
+                careNeed.SubmitterAliasId = ppSubmitter.PersonAliasId;
+
+                careNeed.StatusValueId = dvpStatus.SelectedValue.AsIntegerOrNull();
+                careNeed.CategoryValueId = dvpCategory.SelectedValue.AsIntegerOrNull();
+
+                if ( dpDate.SelectedDate.HasValue )
+                {
+                    careNeed.DateEntered = dpDate.SelectedDate.Value;
+                }
+
+                if ( careNeed.IsValid )
+                {
+                    if ( careNeed.Id.Equals( 0 ) )
+                    {
+                        careNeedService.Add( careNeed );
+                    }
+
+                    // get attributes
+                    careNeed.LoadAttributes();
+                    Helper.GetEditValues( phAttributes, careNeed );
+
+                    rockContext.WrapTransaction( () =>
+                    {
+                        rockContext.SaveChanges();
+                        careNeed.SaveAttributeValues( rockContext );
+                    } );
+
+                    // redirect back to parent
+                    var personId = this.PageParameter( "PersonId" ).AsIntegerOrNull();
+                    var qryParams = new Dictionary<string, string>();
+                    if ( personId.HasValue )
+                    {
+                        qryParams.Add( "PersonId", personId.ToString() );
+                    }
+
+                    NavigateToParentPage( qryParams );
+                }
+            }
+        }
+
+        /// <summary>
+        /// Handles the Click event of the lbCancel control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void lbCancel_Click( object sender, EventArgs e )
+        {
+            var personId = this.PageParameter( "PersonId" ).AsIntegerOrNull();
+            var qryParams = new Dictionary<string, string>();
+            if ( personId.HasValue )
+            {
+                qryParams.Add( "PersonId", personId.ToString() );
+            }
+
+            NavigateToParentPage( qryParams );
+        }
+
+        /// <summary>
+        /// Handles the SelectPerson event of the ppPerson control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void ppPerson_SelectPerson( object sender, EventArgs e )
+        {
+            if ( ppPerson.PersonId != null )
+            {
+                Person person = new PersonService( new RockContext() ).Get( ppPerson.PersonId.Value );
+                if ( person != null )
+                {
+                    if ( !string.IsNullOrWhiteSpace( person.FirstName ) )
+                    {
+                        dtbFirstName.Text = person.FirstName;
+                    }
+                    else if ( !string.IsNullOrWhiteSpace( person.NickName ) )
+                    {
+                        dtbFirstName.Text = person.NickName;
+                    }
+
+                    dtbFirstName.Enabled = string.IsNullOrWhiteSpace( dtbFirstName.Text );
+
+                    dtbLastName.Text = person.LastName;
+                    dtbLastName.Enabled = string.IsNullOrWhiteSpace( dtbLastName.Text );
+
+                    var homePhoneType = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_PHONE_TYPE_HOME.AsGuid() );
+                    if ( homePhoneType != null )
+                    {
+                        var homePhone = person.PhoneNumbers.FirstOrDefault( n => n.NumberTypeValueId == homePhoneType.Id );
+                        if ( homePhone != null )
+                        {
+                            pnbHomePhone.Text = homePhone.NumberFormatted;
+                            pnbHomePhone.Enabled = false;
+                        }
+                    }
+
+                    var mobilePhoneType = DefinedValueCache.Get( Rock.SystemGuid.DefinedValue.PERSON_PHONE_TYPE_MOBILE.AsGuid() );
+                    if ( mobilePhoneType != null )
+                    {
+                        var mobileNumber = person.PhoneNumbers.FirstOrDefault( n => n.NumberTypeValueId == mobilePhoneType.Id );
+                        if ( mobileNumber != null )
+                        {
+                            pnbCellPhone.Text = mobileNumber.NumberFormatted;
+                            pnbCellPhone.Enabled = false;
+                        }
+                    }
+
+                    ebEmail.Text = person.Email;
+                    ebEmail.Enabled = false;
+
+                    lapAddress.SetValue( person.GetHomeLocation() );
+                    lapAddress.Enabled = false;
+
+                    int? requestId = PageParameter( "CareNeedId" ).AsIntegerOrNull();
+
+                    if ( !cpCampus.SelectedCampusId.HasValue && ( e != null || ( requestId.HasValue && requestId == 0 ) ) )
+                    {
+                        var personCampus = person.GetCampus();
+                        cpCampus.SelectedCampusId = personCampus != null ? personCampus.Id : ( int? ) null;
+                    }
+                }
+            }
+            else
+            {
+                dtbFirstName.Enabled = true;
+                dtbLastName.Enabled = true;
+                pnbHomePhone.Enabled = true;
+                pnbCellPhone.Enabled = true;
+                ebEmail.Enabled = true;
+                //lapAddress.Enabled = true;
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Shows the detail.
+        /// </summary>
+        /// <param name="careNeedId">The care need identifier</param>
+        public void ShowDetail( int careNeedId )
+        {
+            CareNeed careNeed = null;
+            var rockContext = new RockContext();
+            CareNeedService careNeedService = new CareNeedService( rockContext );
+            if ( !careNeedId.Equals( 0 ) )
+            {
+                careNeed = careNeedService.Get( careNeedId );
+                pdAuditDetails.SetEntity( careNeed, ResolveRockUrl( "~" ) );
+            }
+
+            if ( careNeed == null )
+            {
+                careNeed = new CareNeed { Id = 0 };
+                careNeed.DateEntered = RockDateTime.Now;
+                var personId = this.PageParameter( "PersonId" ).AsIntegerOrNull();
+                if ( personId.HasValue )
+                {
+                    var person = new PersonService( rockContext ).Get( personId.Value );
+                    if ( person != null )
+                    {
+                        careNeed.PersonAliasId = person.PrimaryAliasId;
+                        careNeed.PersonAlias = person.PrimaryAlias;
+                    }
+                }
+                pdAuditDetails.Visible = false;
+            }
+
+            pnlNewPersonFields.Visible = _allowNewPerson;
+
+            dtbDetailsText.Text = careNeed.Details;
+            dpDate.SelectedDate = careNeed.DateEntered;
+
+            if ( careNeed.Campus != null )
+            {
+                cpCampus.SelectedCampusId = careNeed.CampusId;
+            }
+            else
+            {
+                cpCampus.SelectedIndex = 0;
+            }
+
+            if ( careNeed.PersonAlias != null )
+            {
+                ppPerson.SetValue( careNeed.PersonAlias.Person );
+            }
+            else
+            {
+                ppPerson.SetValue( null );
+            }
+
+            LoadDropDowns( careNeed );
+
+            if ( careNeed.StatusValueId != null )
+            {
+                dvpStatus.SetValue( careNeed.StatusValueId );
+
+                if ( careNeed.Status.Value == "Open" )
+                {
+                    hlStatus.Text = "Open";
+                    hlStatus.LabelType = LabelType.Success;
+                }
+
+                if ( careNeed.Status.Value == "Follow Up" )
+                {
+                    hlStatus.Text = "Follow Up";
+                    hlStatus.LabelType = LabelType.Warning;
+                }
+
+                if ( careNeed.Status.Value == "Closed" )
+                {
+                    hlStatus.Text = "Closed";
+                    hlStatus.LabelType = LabelType.Danger;
+                }
+            }
+
+            if ( careNeed.CategoryValueId != null )
+            {
+                dvpCategory.SetValue( careNeed.CategoryValueId );
+            }
+
+            if ( careNeed.SubmitterPersonAlias != null )
+            {
+                ppSubmitter.SetValue( careNeed.SubmitterPersonAlias.Person );
+            }
+            else if ( CurrentPersonAlias != null )
+            {
+                ppSubmitter.SetValue( CurrentPersonAlias.Person );
+            }
+            else
+            {
+                ppSubmitter.SetValue( null );
+            }
+
+            careNeed.LoadAttributes();
+            Helper.AddEditControls( careNeed, phAttributes, true, BlockValidationGroup, 2 );
+
+            ppPerson_SelectPerson( null, null );
+
+            hfCareNeedId.Value = careNeed.Id.ToString();
+        }
+
+        /// <summary>
+        /// Loads the drop downs.
+        /// </summary>
+        private void LoadDropDowns( CareNeed careNeed )
+        {
+            dvpStatus.DefinedTypeId = DefinedTypeCache.Get( new Guid( rocks.kfs.StepsToCare.SystemGuid.DefinedType.CARE_NEED_STATUS ) ).Id;
+            dvpCategory.DefinedTypeId = DefinedTypeCache.Get( new Guid( rocks.kfs.StepsToCare.SystemGuid.DefinedType.CARE_NEED_CATEGORY ) ).Id;
+
+            ppSubmitter.Visible = true;
+        }
+
+        #endregion
+    }
+}
