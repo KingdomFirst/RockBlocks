4646ba0df1a0bce1ace0959dcfcbf56aefab3bef
guy <guym@kingdomfirstsolutions.com>
2023-05-11 15:41:48 -0400

    Changes from merge review

--- ShelbyFinancials/BatchToJournal.ascx.cs
+++ ShelbyFinancials/BatchToJournal.ascx.cs
@@ -56,12 +56,12 @@ namespace RockWeb.Plugins.rocks_kfs.ShelbyFinancials
 
     [EnumField(
         "GL Account Grouping",
-        Description = "The strategy to follow for grouping GL accounts into a single line.",
+        Description = "Determines if debit and/or credit lines should be grouped and summed by GL account in the export file.",
         IsRequired = true,
         EnumSourceType = typeof( GLEntryGroupingMode ),
-        DefaultEnumValue = ( int ) GLEntryGroupingMode.FinancialAccount,
+        DefaultEnumValue = ( int ) GLEntryGroupingMode.DebitAndCreditByFinancialAccount,
         Order = 2,
-        Key = AttributeKey.AccountGroupngMode )]
+        Key = AttributeKey.AccountGroupingMode )]
 
     [LavaField(
         "Journal Description Lava",
@@ -91,7 +91,7 @@ namespace RockWeb.Plugins.rocks_kfs.ShelbyFinancials
         {
             public const string ButtonText = "ButtonText";
             public const string CloseBatch = "CloseBatch";
-            public const string AccountGroupngMode = "AccountGroupngMode";
+            public const string AccountGroupingMode = "AccountGroupingMode";
             public const string JournalMemoLava = "JournalDescriptionLava";
             public const string EnableDebug = "EnableDebug";
         }
@@ -228,7 +228,7 @@ namespace RockWeb.Plugins.rocks_kfs.ShelbyFinancials
                 var period = tbAccountingPeriod.Text.AsInteger();
 
                 var debugLava = GetAttributeValue( AttributeKey.EnableDebug );
-                var groupingMode = ( GLEntryGroupingMode ) GetAttributeValue( AttributeKey.AccountGroupngMode ).AsInteger();
+                var groupingMode = ( GLEntryGroupingMode ) GetAttributeValue( AttributeKey.AccountGroupingMode ).AsInteger();
 
                 var items = sfJournal.GetGLExcelLines( rockContext, _financialBatch, journalCode, period, ref debugLava, GetAttributeValue( AttributeKey.JournalMemoLava ), groupingMode );
 
