9e51a7c76f915be6b3a6635f8c6f10b1b9cf4ff0
Nate Hoffman <nateh@kingdomfirstsolutions.com>
2024-11-14 10:08:21 -0700

    Add primary logic to keep track of attendance to load/save, update lava template, add a new setting to display group names, and add a search.

--- Groups/GroupAttendanceMulti.ascx
+++ Groups/GroupAttendanceMulti.ascx
@@ -27,7 +27,10 @@
             }
 
             .card-checkbox .checkbox label .label-text {
-                border: solid 1px #000;
+                --checked-border-color: var(--color-primary,#ee7525);
+                --checked-background-color: rgba(var(--color-base-primary),0.1);
+                background-color: var(--background-color);
+                border: 1px solid var(--border-color,var(--input-border));
                 display: block;
                 clear: both;
                 padding: .5em;
@@ -35,9 +38,16 @@
                 width: 100%;
             }
 
+                .card-checkbox .checkbox label .label-text small {
+                    font-size: x-small;
+                    display: block;
+                }
+
             .card-checkbox .checkbox label input:checked + .label-text {
-                border-color: red;
-                background-color: rgba(0,0,0,0.1);
+                --border-color: var(--checked-border-color);
+                --background-color: var(--checked-background-color);
+                outline: 2px solid var(--checked-border-color);
+                outline-offset: -2px
             }
 </style>
 <asp:UpdatePanel ID="pnlContent" runat="server">
@@ -48,24 +58,35 @@
             <div class="panel-heading">
                 <h1 class="panel-title pull-left">
                     <i class="fa fa-check-square-o"></i>
-                    <asp:Literal ID="lHeading" runat="server" Text="Group Attendance" />
+                    <asp:Literal ID="lHeading" runat="server" Text="Multiple Group Attendance" />
                 </h1>
             </div>
 
             <div class="panel-body">
+                <Rock:NotificationBox ID="nbNotice" runat="server" />
+                <asp:ValidationSummary ID="ValidationSummary1" runat="server" HeaderText="Please correct the following:" CssClass="alert alert-validation" />
+                <asp:CustomValidator ID="cvAttendance" runat="server" Display="None" />
 
-                <div class="row">
+                <div class="row mb-5">
                     <asp:Panel ID="pnlDate" runat="server" CssClass="col-sm-6">
-                        <strong>Enter Attendance For:</strong>
-                        <asp:Literal ID="lAttendanceDate" runat="server"></asp:Literal>
+                        <Rock:RockLiteral ID="lAttendanceDate" runat="server" Label="Enter Attendance For" />
+                        <Rock:DatePicker ID="dpAttendanceDate" runat="server" Label="Enter Attendance For" AllowFutureDateSelection="false" Required="true" OnSelectDate="dpAttendanceDate_SelectDate" AutoPostBack="true" />
                     </asp:Panel>
-                    <asp:Panel ID="pnlOther" runat="server" class="col-sm-6"></asp:Panel>
+                    <asp:Panel ID="pnlOther" runat="server" class="col-sm-6">
+                        <Rock:RockLiteral ID="lSchedule" runat="server" Label="Schedule(s)" />
+                    </asp:Panel>
+                </div>
+                <div class="row mb-3">
+                    <asp:Panel ID="pnlSearch" runat="server" CssClass="col-xs-12 col-sm-10">
+                        <Rock:RockTextBox ID="tbSearch" runat="server" OnTextChanged="tbSearch_TextChanged" AutoPostBack="true" Placeholder="Search"></Rock:RockTextBox>
+                    </asp:Panel>
                 </div>
                 <asp:Panel ID="pnlAttendance" runat="server" CssClass="d-flex flex-wrap">
                     <asp:Repeater ID="rptrAttendance" runat="server">
                         <ItemTemplate>
+                            <asp:HiddenField ID="hdnAttendeeId" runat="server" Value='<%# Eval("PersonId") %>' />
                             <asp:Panel ID="pnlCardCheckbox" runat="server" CssClass="card-checkbox">
-                                <Rock:RockCheckBox ID="cbAttendee" runat="server" CssClass="attendeeCheckbox" />
+                                <Rock:RockCheckBox ID="cbAttendee" runat="server" CssClass="attendeeCheckbox" Checked='<%# Eval("Attended") %>' OnCheckedChanged="cbAttendee_CheckedChanged" AutoPostBack="true" />
                             </asp:Panel>
                         </ItemTemplate>
                     </asp:Repeater>
--- Groups/GroupAttendanceMulti.ascx.cs
+++ Groups/GroupAttendanceMulti.ascx.cs
@@ -17,13 +17,15 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Web.UI.WebControls;
+
 using Rock;
 using Rock.Attribute;
 using Rock.Data;
-using Rock.Lava;
 using Rock.Model;
+using Rock.Security;
 using Rock.Web.UI;
 using Rock.Web.UI.Controls;
 
@@ -74,6 +76,11 @@ namespace Plugins.rocks_kfs.Groups
         DefaultValue = "col-xs-12 col-sm-6 col-md-3 col-lg-2",
         Key = AttributeKey.CheckboxColumnClass )]
 
+    [BooleanField( "Display Group Names",
+        Description = "Display the group names in the panel title.",
+        DefaultBooleanValue = true,
+        Key = AttributeKey.DisplayGroupNames )]
+
     [Rock.SystemGuid.BlockTypeGuid( "B8724DBC-F8FB-426D-9296-87A5944273B9" )]
     public partial class GroupAttendanceMulti : RockBlock
     {
@@ -85,23 +92,52 @@ namespace Plugins.rocks_kfs.Groups
             public const string GroupsToDisplay = "GroupsToDisplay";
             public const string AttendeeLavaTemplate = "AttendeeLavaTemplate";
             public const string CheckboxColumnClass = "CheckboxColumnClass";
+            public const string DisplayGroupNames = "DisplayGroupNames";
         }
 
         private static class DefaultValue
         {
-            public const string AttendeeLavaTemplate = @"<img src=""{{ GroupMember.Person.PhotoUrl }}"" class=""img-circle col-xs-3 p-0 mr-3 pull-left""> {{ GroupMember.Person.FullName }}";
+            public const string AttendeeLavaTemplate = @"{% comment %}
+  This is the lava template for each attendee item in the GroupAttendanceMulti block
+   Available Lava Fields:
+
+   + Person (the person from the group)
+   + Attended (whether or not the person is marked DidAttend = true)
+   + GroupMembers (the group member records for the Person)
+   + Groups (the group(s) the person is a member of)
+{% endcomment %}
+<img src=""{{ Person.PhotoUrl }}"" class=""img-circle col-xs-3 p-0 mr-3 pull-left""> {{ Person.FullName }}<br>
+<small class=""text-muted"">{{ Groups | Join:', ' }}</small>";
+        }
+
+        private static class PageParameterKey
+        {
+            public const string Date = "Date";
+            public const string Occurrence = "Occurrence";
         }
 
         #region Private Variables
 
         private RockContext _rockContext = null;
-        private bool _canView = false;
         private List<Group> _groups = new List<Group>();
         private List<GroupMember> _members = new List<GroupMember>();
+        private List<AttendanceAttendee> _attendees;
+        private DateTime? _attendanceDate = null;
 
         #endregion
 
         #region Control Methods
+
+        protected override void LoadViewState( object savedState )
+        {
+            base.LoadViewState( savedState );
+            _attendees = ViewState["Attendees"] as List<AttendanceAttendee>;
+        }
+        protected override object SaveViewState()
+        {
+            ViewState["Attendees"] = _attendees;
+            return base.SaveViewState();
+        }
 
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Init" /> event.
@@ -113,18 +149,20 @@ namespace Plugins.rocks_kfs.Groups
 
             this.BlockUpdated += Block_BlockUpdated;
             this.AddConfigurationUpdateTrigger( pnlContent );
+            tbSearch.Attributes["onkeypress"] = $"clearTimeout(window.{tbSearch.ClientID}); window.{tbSearch.ClientID} = setTimeout('__doPostBack(\\'{tbSearch.UniqueID}\\',\\'\\')', 1000)";
 
             _rockContext = new RockContext();
 
             var groupIds = GetAttributeValues( AttributeKey.GroupsToDisplay ).AsIntegerList();
             _groups = new GroupService( _rockContext ).GetByIds( groupIds ).ToList();
 
-            foreach ( var groupId in groupIds )
+            foreach ( var group in _groups )
             {
-                _members.AddRange( new GroupMemberService( _rockContext ).GetByGroupId( groupId ).ToList() );
+                if ( group != null && ( group.IsAuthorized( Authorization.MANAGE_MEMBERS, CurrentPerson ) || group.IsAuthorized( Authorization.EDIT, CurrentPerson ) ) )
+                {
+                    _members.AddRange( group.ActiveMembers() );
+                }
             }
-
-            _canView = true;
         }
         /// <summary>
         /// Raises the <see cref="E:System.Web.UI.Control.Load" /> event.
@@ -134,12 +172,32 @@ namespace Plugins.rocks_kfs.Groups
         {
             base.OnLoad( e );
 
-            pnlContent.Visible = _canView;
-
-            if ( !Page.IsPostBack && _canView )
+            if ( !Page.IsPostBack )
             {
-                BindFilter();
+                BindFields();
                 BindRepeat();
+            }
+            else
+            {
+                if ( _attendees != null )
+                {
+                    foreach ( RepeaterItem item in rptrAttendance.Items )
+                    {
+                        var hdnAttendeeId = item.FindControl( "hdnAttendeeId" ) as HiddenField;
+                        var cbAttendee = item.FindControl( "cbAttendee" ) as RockCheckBox;
+
+                        if ( hdnAttendeeId != null && cbAttendee != null )
+                        {
+                            int personId = hdnAttendeeId.ValueAsInt();
+
+                            var attendance = _attendees.Where( a => a.PersonId == personId ).FirstOrDefault();
+                            if ( attendance != null )
+                            {
+                                attendance.Attended = cbAttendee.Checked;
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -154,56 +212,289 @@ namespace Plugins.rocks_kfs.Groups
         /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
         protected void Block_BlockUpdated( object sender, EventArgs e )
         {
-            BindFilter();
+            BindFields();
             BindRepeat();
         }
 
+        /// <summary>
+        /// Handles the SelectDate event of the dpAttendanceDate control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void dpAttendanceDate_SelectDate( object sender, EventArgs e )
+        {
+            BindFields();
+            BindRepeat();
+        }
+
+        /// <summary>
+        /// Handles the TextChanged event of the tbSearch control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void tbSearch_TextChanged( object sender, EventArgs e )
+        {
+            BindFields();
+            BindRepeat();
+            tbSearch.Focus();
+        }
+
+        /// <summary>
+        /// Handles the ItemDataBound event of the RptrAttendance control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Web.UI.WebControls.RepeaterItemEventArgs"/> instance containing the event data.</param>
         private void RptrAttendance_ItemDataBound( object sender, System.Web.UI.WebControls.RepeaterItemEventArgs e )
         {
-            var groupMember = e.Item.DataItem as GroupMember;
+            var attendee = e.Item.DataItem as AttendanceAttendee;
             var cbAttendee = e.Item.FindControl( "cbAttendee" ) as RockCheckBox;
             var pnlCardCheckbox = e.Item.FindControl( "pnlCardCheckbox" ) as Panel;
 
-            if ( groupMember != null && cbAttendee != null && pnlCardCheckbox != null )
+            if ( attendee != null && cbAttendee != null && pnlCardCheckbox != null )
             {
                 pnlCardCheckbox.AddCssClass( GetAttributeValue( AttributeKey.CheckboxColumnClass ) );
+                cbAttendee.Checked = attendee.Attended;
 
                 var lavaTemplate = GetAttributeValue( AttributeKey.AttendeeLavaTemplate );
 
                 var mergeFields = Rock.Lava.LavaHelper.GetCommonMergeFields( this.RockPage, this.CurrentPerson );
-                mergeFields.Add( "GroupMember", groupMember );
+                mergeFields.Add( "Person", _members.Where( gm => gm.PersonId == attendee.PersonId ).Select( gm => gm.Person ).FirstOrDefault() );
+                mergeFields.Add( "Attended", attendee.Attended );
+                mergeFields.Add( "GroupMembers", _members.Where( gm => attendee.GroupMemberIds.Contains( gm.Id ) ) );
+                mergeFields.Add( "Groups", _groups.Where( g => attendee.Groups.Contains( g.Id ) ) );
 
                 cbAttendee.Text = lavaTemplate.ResolveMergeFields( mergeFields );
             }
         }
 
+        /// <summary>
+        /// Handles the CheckedChanged event of the cbAttendee control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        protected void cbAttendee_CheckedChanged( object sender, EventArgs e )
+        {
+            var cbAttendee = sender as RockCheckBox;
+
+            if ( cbAttendee != null )
+            {
+                SaveAttendance();
+            }
+        }
 
         #endregion
 
         #region Internal Methods
 
         /// <summary>
-        /// Binds the filter.
+        /// Binds the fields.
         /// </summary>
-        protected void BindFilter()
+        protected void BindFields()
         {
+            var displayGroupName = GetAttributeValue( AttributeKey.DisplayGroupNames ).AsBoolean();
+            if ( displayGroupName )
+            {
+                lHeading.Text = $"{BlockName} - {_groups.Select( g => g.Name ).JoinStringsWithCommaAnd()}";
+            }
+            else
+            {
+                lHeading.Text = BlockName;
+            }
 
+            if ( !_members.Any() )
+            {
+                nbNotice.Text = "There are no members to display for the selected group(s). Please check if you have permission to Manage Members or Edit the group(s) selected.";
+            }
+
+            _attendanceDate = PageParameter( PageParameterKey.Date ).AsDateTime() ?? PageParameter( PageParameterKey.Occurrence ).AsDateTime();
+
+            if ( _attendanceDate.HasValue )
+            {
+                lAttendanceDate.Text = _attendanceDate.ToShortDateString();
+                lAttendanceDate.Visible = true;
+                dpAttendanceDate.Visible = false;
+            }
+            else
+            {
+                dpAttendanceDate.SelectedDate = dpAttendanceDate.SelectedDate ?? RockDateTime.Now;
+                _attendanceDate = dpAttendanceDate.SelectedDate;
+                dpAttendanceDate.Visible = true;
+                lAttendanceDate.Visible = false;
+            }
+
+            lSchedule.Text = _groups.Where( g => g.Schedule != null ).ToList().Select( g => g.Schedule.FriendlyScheduleText ).JoinStrings( ", " );
         }
 
         /// <summary>
-        /// Binds the group members grid.
+        /// Binds the group members/attendees repeater.
         /// </summary>
         protected void BindRepeat()
         {
+            var attendedIds = new List<int>();
+            using ( var rockContext = new RockContext() )
+            {
+                var groupIds = _groups.Select( g => g.Id ).ToList();
+                attendedIds = new AttendanceService( rockContext )
+                    .Queryable().AsNoTracking()
+                    .Where( a =>
+                        DbFunctions.DiffDays( a.StartDateTime, _attendanceDate ) == 0 &&
+                        a.DidAttend.HasValue &&
+                        a.DidAttend.Value &&
+                        a.Occurrence != null &&
+                        groupIds.Contains( a.Occurrence.GroupId.Value ) &&
+                        a.PersonAlias != null )
+                    .Select( a => a.PersonAlias.PersonId )
+                    .Distinct()
+                    .ToList();
+            }
+            var searchParts = tbSearch.Text.ToLower().SplitDelimitedValues();
+            _attendees = _members
+                .Where( gm => tbSearch.Text.IsNullOrWhiteSpace() ||
+                      ( searchParts.Length == 1 && gm.Person.LastName.ToLower().StartsWith( searchParts[0] ) ) ||
+                      ( searchParts.Length > 1 && gm.Person.FirstName.ToLower().StartsWith( searchParts[0] ) && gm.Person.LastName.ToLower().StartsWith( searchParts[1] ) )
+                )
+                .OrderBy( gm => gm.Person.LastName )
+                                                    .ThenBy( gm => gm.Person.FirstName )
+                                                    .ThenBy( gm => gm.Group.Name )
+                                                    .GroupBy( gm => gm.PersonId )
+                                                    .Select( g => new AttendanceAttendee
+                                                    {
+                                                        PersonId = g.Key,
+                                                        GroupMemberIds = g.Select( gm => gm.Id ).ToList(),
+                                                        Attended = attendedIds.Contains( g.Key ),
+                                                        Groups = g.Select( gm => gm.GroupId ).ToList()
+                                                    } )
+                                                    .ToList();
+
             rptrAttendance.ItemDataBound += RptrAttendance_ItemDataBound;
-            rptrAttendance.DataSource = _members.OrderBy( gm => gm.Person.LastName )
-                                                .ThenBy( gm => gm.Person.FirstName )
-                                                .ThenBy( gm => gm.Group.Name );
+            rptrAttendance.DataSource = _attendees;
             rptrAttendance.DataBind();
+        }
+
+        /// <summary>
+        /// Saves the attendance.
+        /// </summary>
+        /// <returns>boolean</returns>
+        private bool SaveAttendance()
+        {
+            using ( var rockContext = new RockContext() )
+            {
+                var occurrenceService = new AttendanceOccurrenceService( rockContext );
+                var attendanceService = new AttendanceService( rockContext );
+                var personAliasService = new PersonAliasService( rockContext );
+                var occurrences = new Dictionary<string, AttendanceOccurrence>();
+
+                if ( dpAttendanceDate.Visible && dpAttendanceDate.SelectedDate.HasValue )
+                {
+                    _attendanceDate = dpAttendanceDate.SelectedDate.Value;
+                }
+                if ( !_attendanceDate.HasValue )
+                {
+                    nbNotice.Text = "You must choose a date before selecting a person to record attendance.";
+                    return false;
+                }
+                if ( _attendees != null )
+                {
+                    foreach ( var attendee in _attendees )
+                    {
+                        var attendeeGroups = _groups.Where( g => attendee.Groups.Contains( g.Id ) );
+                        foreach ( var group in attendeeGroups )
+                        {
+                            var scheduleId = group.ScheduleId;
+                            AttendanceOccurrence occurrence = null;
+
+                            var occurrenceKey = $"{group.Id}_{scheduleId}";
+
+                            if ( occurrence == null && occurrences.ContainsKey( occurrenceKey ) )
+                            {
+                                occurrence = occurrences.GetValueOrNull( occurrenceKey );
+                            }
+
+                            if ( occurrence == null && _attendanceDate.HasValue )
+                            {
+                                occurrence = occurrenceService.Get( _attendanceDate.Value.Date, group.Id, null, scheduleId );
+                            }
+
+                            if ( occurrence == null )
+                            {
+                                occurrence = new AttendanceOccurrence();
+                                occurrence.GroupId = group.Id;
+                                occurrence.ScheduleId = scheduleId;
+                                occurrence.OccurrenceDate = _attendanceDate.Value;
+                                occurrenceService.Add( occurrence );
+                            }
+
+                            var existingAttendees = occurrence.Attendees.ToList();
+
+                            int? campusId = group.CampusId;
+
+                            occurrence.Schedule = occurrence.Schedule == null && occurrence.ScheduleId.HasValue ? new ScheduleService( rockContext ).Get( occurrence.ScheduleId.Value ) : occurrence.Schedule;
+
+                            cvAttendance.IsValid = occurrence.IsValid;
+                            if ( !cvAttendance.IsValid )
+                            {
+                                cvAttendance.ErrorMessage = occurrence.ValidationResults.Select( a => a.ErrorMessage ).ToList().AsDelimited( "<br />" );
+                                return false;
+                            }
+
+                            var attendance = existingAttendees
+                                .Where( a => a.PersonAlias != null && a.PersonAlias.PersonId == attendee.PersonId )
+                                .FirstOrDefault();
+
+                            if ( attendance == null )
+                            {
+                                int? personAliasId = personAliasService.GetPrimaryAliasId( attendee.PersonId );
+                                if ( personAliasId.HasValue )
+                                {
+                                    attendance = new Attendance();
+                                    attendance.PersonAliasId = personAliasId;
+                                    attendance.CampusId = campusId;
+                                    attendance.StartDateTime = occurrence.Schedule != null && occurrence.Schedule.HasSchedule() ? occurrence.OccurrenceDate.Date.Add( occurrence.Schedule.StartTimeOfDay ) : occurrence.OccurrenceDate;
+                                    attendance.DidAttend = attendee.Attended;
+
+                                    cvAttendance.IsValid = attendance.IsValid;
+                                    if ( !cvAttendance.IsValid )
+                                    {
+                                        cvAttendance.ErrorMessage = attendance.ValidationResults.Select( a => a.ErrorMessage ).ToList().AsDelimited( "<br />" );
+                                        return false;
+                                    }
+
+                                    occurrence.Attendees.Add( attendance );
+                                }
+                            }
+                            else
+                            {
+                                attendance.DidAttend = attendee.Attended;
+                            }
+
+                            occurrences.AddOrReplace( occurrenceKey, occurrence );
+                        }
+                    }
+                    rockContext.SaveChanges();
+
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
         }
 
         #endregion
     }
 
+    [Serializable]
+    public class AttendanceAttendee
+    {
+        public int PersonId { get; set; }
 
+        public List<int> GroupMemberIds { get; set; }
+
+        public bool Attended { get; set; } = false;
+
+        public List<int> Groups { get; set; }
+
+
+    }
 }
