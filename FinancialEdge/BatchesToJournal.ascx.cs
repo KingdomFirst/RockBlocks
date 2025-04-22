963a2c9f7aae92933f0f0822786ec615109539eb
Guy Maness <guy.maness@gmail.com>
2025-04-22 12:22:11 -0400

    Merge pull request #199 from KingdomFirst/bug/nbh-fixWithV17Updates
    Financial Edge and Shelby Financials minor bug fix
    
    (cherry picked from commit 744133ae26688b2b5ef1ae5fd149fe182d650ae2)

--- FinancialEdge/BatchesToJournal.ascx.cs
+++ FinancialEdge/BatchesToJournal.ascx.cs
@@ -459,10 +459,8 @@ namespace RockWeb.Plugins.rocks_kfs.FinancialEdge
 
                     batch.LoadAttributes();
 
-                    var newDate = string.Empty;
-
                     var oldDate = batch.GetAttributeValue( "rocks.kfs.FinancialEdge.DateExported" );
-                    newDate = RockDateTime.Now.ToString();
+                    var newDate = RockDateTime.Now;
                     History.EvaluateChange( changes, "Date Exported", oldDate, newDate.ToString() );
 
                     items.AddRange( feJournal.GetGlEntries( rockContext, batch, tbJournalType.Text ) );
